03f70535e3f5080110dcd1999378170499f8b84e
David Baas <Dcbaas@users.noreply.github.com>
2020-02-05 22:30:42 -0500

    started gravity.py

---
+++ neon-souls/physics/gravity.py
@@ -0,0 +1,33 @@
+import abc
+from threading import Lock
+
+class GravityManager:
+    gravity_instance = None
+    lock = Lock()
+
+    def __init__(self):
+        raise RuntimeError('Use get_instance')
+    
+    @classmethod
+    def get_instance(cls):
+        if cls.gravity_instance is None:
+            cls.lock.acquire()
+            if cls.gravity_instance is None:
+                cls.gravity_instance = cls.__new__(cls)
+                cls.gravity_instance.gravity_map = {}
+            cls.lock.release()
+        return cls.gravity_instance
+
+    def add_gravity(self, gravity_name, gravity_vector):
+        self.gravity_map[gravity_name] = gravity_vector
+
+    def get_gravity(self, gravity_name)
+        self.gravity_map.get(gravity_name, (0, 0))
+
+class GravityBound(abc.ABC):
+    @abc.abstractmethod
+    def process_gravity(self):
+        pass
+    
+
+        
