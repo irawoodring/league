ff8e705939610dec93805c6cbf97649ea9b0f375
Jesse David <davidjes@mail.gvsu.edu>
2020-02-07 08:32:43 -0500

    Sprites and maps and stuff

---
+++ assets/bee.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="1" tilewidth="456" tileheight="345" tilecount="1" columns="1">
+ <image source="../enemies/PNG/1.png" width="456" height="345"/>
+</tileset>
---
+++ assets/spider01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="spider01" tilewidth="64" tileheight="64" tilecount="50" columns="10">
+ <image source="../enemies/LPC_Spiders/spider01.png" width="640" height="320"/>
+</tileset>
