aa0f010a8e7a968c5f2069c98f2ed765b5297aff
Sierra Ellison <ellisosi@mail.gvsu.edu>
2020-02-14 00:14:31 -0500

    projectile class

---
+++ neon-souls/projectiles.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Projectile:
+    def __init__(self, x, y, radius, facing, image_path):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.facing = facing
+        self.image_path = self.image_path
+        #self.velocity = 8 * facing
+    
+    
