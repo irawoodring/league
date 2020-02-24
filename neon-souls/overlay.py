5724b2b7ee2ff7a5b028779dd8a784008e3a165b
Sierra Ellison <ellisosi@mail.gvsu.edu>
2020-02-24 02:52:44 -0500

    as a quick fix, update overlay x to equal player x so health stays on screen

--- neon-souls/overlay.py
+++ neon-souls/overlay.py
@@ -7,10 +7,10 @@ class Overlay(league.DUGameObject):
         self._layer = 1000
         self.player = player
         self.font = pygame.font.Font('freesansbold.ttf',32)
-        self.image = pygame.Surface([800, 40])
+        self.image = pygame.Surface([6000, 40])
         self.image.fill((127, 127, 127))
-        self.text = self.font.render("    " + str(self.player.health.current_health) + " HP", True, (0,0,0))
-        self.image.blit(self.text, (0, 0))
+        self.text = self.font.render(str(self.player.health.current_health) + " HP", True, (0,0,0))
+        self.image.blit(self.text, (self.player.rect.centerx, 0))
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
@@ -20,5 +20,5 @@ class Overlay(league.DUGameObject):
 
     def update(self, deltaTime):
         self.image.fill((127, 127, 127))
-        self.text = self.font.render("    " + str(self.player.health.current_health) + " HP", True, (0,0,0))
-        self.image.blit(self.text, (0, 0))
+        self.text = self.font.render(str(self.player.health.current_health) + " HP", True, (0,0,0))
+        self.image.blit(self.text, (self.player.rect.centerx, 0))
